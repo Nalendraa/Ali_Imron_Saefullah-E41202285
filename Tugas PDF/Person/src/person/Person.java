158327a944350880308d18dcf7087b97aa574e11
Nalendraa <shawnaxcel42@gmail.com>
2021-03-15 22:05:54 +0700

    Create Person.java

---
+++ Tugas PDF/Person/src/person/Person.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package person;
+
+/**
+ *
+ * @author E41202285 Ali Imron Saefullah
+ */
+public class Person {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // Data
+        String fName = "Ali Imron Saefullah";
+        String IName = "Alii";
+        int Usia = 19;
+        String stuId = "E412022285";
+        String stuStatus = "Active";
+
+        // Pemanggilan Data
+        System.out.println(fName);
+        System.out.println(IName);
+        System.out.println(Usia);
+        System.out.println(stuId);
+        System.out.println(stuStatus);
+
+    }
+
+}
