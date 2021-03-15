972bea24aca7805ef6111bde4e8573d4816707a3
Nalendraa <shawnaxcel42@gmail.com>
2021-03-15 16:59:43 +0700

    Create TUGAS_B.java

---
+++ Week 1/TUGAS_B/src/tugas_b/TUGAS_B.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas_b;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class TUGAS_B {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nama = "Politeknik Negeri Jember";
+        char abjad = 'A';
+        double b = 9.9;
+        
+        System.out.println  (nama);
+        System.out.println  (b);
+        
+    }
+    
+}
